185e0e5cab2d2d49fb6b919dc7f037a22c70eb1f
Yaroslav <yarikvavilin@gmail.com>
2020-12-16 22:24:09 +0500

    lint setup

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import readlineSync from 'readline-sync';
+import getName from '../src/cli.js';
+
+const name = readlineSync.question('May I have your name? ');
+getName(name);
---
+++ src/cli.js
@@ -0,0 +1,4 @@
+console.log('Welcome to the Brain Games!');
+export default (name) => {
+  console.log(`Hello, ${name}!`);
+};
