f1602b55b02060538d883dab0a11cb95dda48e04
Yaroslav <yarikvavilin@gmail.com>
2021-01-12 00:03:38 +0500

    fix progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -13,13 +13,53 @@ export default () => {
     }
     const sortProgression = arrayForProgression.sort((a, b) => a - b);
     const randomSplice = Math.floor(Math.random() * sortProgression.length);
-    sortProgression.splice(randomSplice, 1, '..');
+    const s = sortProgression.splice(randomSplice, 1, '..');
     return sortProgression.join(' ');
   };
   const parseExpressionFromString = (progression) => progression.split(' ');
+
   const getRightAnswer = (progression) => {
-    return 'a'
-  }
+    const array = parseExpressionFromString(progression);
+    const parse = array
+      .map((string) => parseInt(string, 10))
+      .filter((num) => Number.isInteger(num));
+    console.log(parse);
+
+    const findMissingUtil = (arr, low, high, diff) => {
+      if (high <= low) {
+        return Number.MAX_SAFE_INTEGER;
+      }
+      const mid = low + (high - low) / 2;
+      // console.log(mid);
+      if (arr[mid + 1] - arr[mid] !== diff) {
+        return (arr[mid] + diff);
+      }
+      if (mid > 0 && arr[mid] - arr[mid - 1] !== diff) {
+        return (arr[mid - 1] + diff);
+      }
+      if (arr[mid] === arr[0] + mid * diff) {
+        return findMissingUtil(arr, mid + 1, high, diff);
+      }
+      return findMissingUtil(arr, low, mid - 1, diff);
+    };
+
+    const findMissing = (arr, n) => {
+      // Если отсутствует только один элемент, то
+      // мы можем найти разницу арифметики
+      // прогрессия по следующей формуле.
+      // Пример, 2, 4, 6, 10, diff = (10-2) / 4 = 2.
+      // В формуле предполагается, что
+      // разница - это целое число
+      const diff = (arr[n - 1] - arr[0]) / n;
+      // Двоичный поиск пропущенного числа
+      // используя выше вычисленный diff
+      return findMissingUtil(arr, 0, n - 1, diff);
+    };
+    const n = parse.length;
+    const find = (findMissing(parse, n));
+    // console.log(find.toString());
+    return find.toString();
+  };
 
   return engine.loop('What number is missing in the progression?', createArithmeticProgression, getRightAnswer);
 };
